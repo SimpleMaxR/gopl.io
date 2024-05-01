27ce59f39447cdaf9335fd3c5f1a78be0e637b32
SimpleMaxR <hugoho912@outlook.com>
2024-05-01 11:55:05 +0800

    update sha256.go

--- ch4/sha256/sha256.go
+++ ch4/sha256/sha256.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	c1 := sha256.Sum256([]byte("X"))
 	c2 := sha256.Sum256([]byte("X"))
-	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
+	fmt.Printf("%#x\n%#x\n%t\n%T\n", c1, c2, c1 == c2, c1)
 }
